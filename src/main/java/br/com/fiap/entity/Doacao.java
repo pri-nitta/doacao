a86b0775eeb1bb5478bd03d8d48e0bed6388586a
Priscila Nitta <priscila.nitta@gmail.com>
2022-03-10 00:22:52 -0300

    centro coleta's parameters length addition

--- src/main/java/br/com/fiap/entity/Doacao.java
+++ src/main/java/br/com/fiap/entity/Doacao.java
@@ -15,45 +15,43 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-
 @Entity
-@Table(name="TB_DOACAO")
+@Table(name = "TB_DOACAO")
 public class Doacao {
-	
+
 	// Atributos
-	
+
 	@Id
-	@Column(name="id_doacao")
-	@SequenceGenerator(name="doacao", sequenceName="sq_tb_doacao", allocationSize=1)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="doacao")
+	@Column(name = "id_doacao", nullable = false)
+	@SequenceGenerator(name = "doacao", sequenceName = "sq_tb_doacao", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "doacao")
 	private int idDoacao;
-	
+
 	@CreationTimestamp
-	@Column(name="dt_ult_coleta", nullable=false)
+	@Column(name = "dt_ult_coleta", nullable = false)
 	private Calendar dtUltColeta;
-	
-	@Column(name="volume_coleta", nullable=false)
+
+	@Column(name = "volume_coleta", nullable = false, length = 6)
 	private String volumeColeta;
-	
-	@Column (name="descricao", nullable= false)
+
+	@Column(name = "descricao", nullable = false, length = 100)
 	private String descricao;
-	
-	
-	// Relacao 
-	
-	@ManyToOne (cascade=CascadeType.PERSIST)  
-	@JoinColumn(name="id_doador")
+
+	// Relacao
+
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "id_doador")
 	private Doador doadores;
-	
-	@ManyToOne (cascade=CascadeType.PERSIST)  
-	@JoinColumn(name="id_centro_col")
+
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "id_centro_col")
 	private CentroColeta coletas;
 
-	
-	// Contrutores 
-	
-	public Doacao(){}
-	
+	// Contrutores
+
+	public Doacao() {
+	}
+
 	public Doacao(int idDoacao, Calendar dtUltColeta, String volumeColeta, String descricao, Doador doadores,
 			CentroColeta coletas) {
 		super();
@@ -64,9 +62,9 @@ public class Doacao {
 		this.doadores = doadores;
 		this.coletas = coletas;
 	}
-	
-	// Getters and Settes
-	
+
+	// Getters and Setters
+
 	public int getIdDoacao() {
 		return idDoacao;
 	}
